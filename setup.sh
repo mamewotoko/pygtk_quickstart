6ca55dd0fbf9007b24ed3b016543ce98831081b6
Takashi Masuyama <mamewotoko@gmail.com>
2020-01-18 07:53:06 +0900

    execute brew as user

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ if [ "$(uname)" = Darwin ]; then
     brew install pygobject3 gtk+3
 elif [ -f /etc/lsb-release ]; then
     # debian, ubuntu
-    apt-get install -y python3 python3-dev python3-pip libgtk-3-dev
+    sudo apt-get install -y python3 python3-dev python3-pip libgtk-3-dev
 else
     echo unsupported platform
     exit 1
