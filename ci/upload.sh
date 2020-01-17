85613ac2a0b206bc9027d5a6d2c4e08f17190985
Takashi Masuyama <mamewotoko@gmail.com>
2020-01-17 22:14:51 +0900

    modify sudo

--- ci/upload.sh
+++ ci/upload.sh
@@ -4,6 +4,6 @@
 COMMIT=$(echo $TRAVIS_COMMIT | sed 's!^\(.\{10\}\).*!\1!')
 
 ARCHIVE=output
-./gdrive upload -r --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR \
+./gdrive upload -r --refresh-token "$GDRIVE_REFRESH_TOKEN" --parent "$GDRIVE_DIR" \
          --description "output-${COMMIT} ${TRAVIS_JOB_WEB_URL}" \
-         $ARCHIVE
+         "$ARCHIVE"
--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 if [ "$(uname)" = Darwin ]; then
+    brew update
     brew install pygobject3 gtk+3
 elif [ -f /etc/lsb-release ]; then
     # debian, ubuntu
-    sudo apt install -y python3 python3-dev python3-pip libgtk-3-dev
+    apt-get install -y python3 python3-dev python3-pip libgtk-3-dev
 else
     echo unsupported platform
     exit 1
